747979305a7d34ba9b1cba71491a86f1b03692e0
Himanshu Kumar Gola <himanshugola1111@gmail.com>
2024-09-14 23:56:16 +0530

    clerk auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,14 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -24,12 +32,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased w-full h-screen grid place-items-center`}
       >
-        {children}
+
+          {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
